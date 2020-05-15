5f0da47755241997bc70c5d632fe9cf9c5c69cdf
Lior Kummer <LKummer41@gmail.com>
2020-05-15 23:19:10 +0300

    Add Stylelint Order plugin and configure it

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,5 +4,15 @@ module.exports = {
     'stylelint-config-recommended-scss',
     'stylelint-config-prettier'
   ],
-  plugins: ['stylelint-scss']
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: {
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'at-rules',
+      'declarations',
+      'rules'
+    ],
+    'order/properties-alphabetical-order': true
+  }
 };
