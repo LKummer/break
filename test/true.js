5d0fe4771c27c5ff3f199c5e765e24599e8d58d4
Lior Kummer <LKummer41@gmail.com>
2020-05-15 23:02:22 +0300

    Format files.

--- test/true.js
+++ test/true.js
@@ -1,10 +1,8 @@
 const { join } = require('path');
 const sassTrue = require('sass-true');
 
-[
-  'test-true.scss'
-]
-.map(file => join(__dirname, file))
-.forEach(file => {
-  sassTrue.runSass({ file }, { describe, it })
-})
+['test-true.scss']
+  .map((file) => join(__dirname, file))
+  .forEach((file) => {
+    sassTrue.runSass({ file }, { describe, it });
+  });
