d4c8c183afaaa84149cfc6d822e6085a6fde5f7a
Lior Kummer <LKummer41@gmail.com>
2020-05-15 22:58:12 +0300

    Add Sass unit test runner.
    Set up True with the Mocha runner.

---
+++ test/true.js
@@ -0,0 +1,10 @@
+const { join } = require('path');
+const sassTrue = require('sass-true');
+
+[
+  'test-true.scss'
+]
+.map(file => join(__dirname, file))
+.forEach(file => {
+  sassTrue.runSass({ file }, { describe, it })
+})
