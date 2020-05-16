ffe864929447301779509399d1a61c42c18ce7d7
Lior Kummer <LKummer41@gmail.com>
2020-05-17 01:12:15 +0300

    Remove unneeded test file.

--- test/true.js
+++ test/true.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 const sassTrue = require('sass-true');
 
-['test-true.scss', 'test-break.scss']
+['test-break.scss']
   .map((file) => join(__dirname, file))
   .forEach((file) => {
     sassTrue.runSass({ file }, { describe, it });
