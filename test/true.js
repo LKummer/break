83998582ba3fa0fea3cb487e2a73bcb49810dae1
Lior Kummer <LKummer41@gmail.com>
2020-05-16 14:17:07 +0300

    Add breakpoint mixins and partial tests.
    Add mixins for creating responsive media queries with a set of pre
    defined breakpoints.
    Add tests for the breakpoint-down and breakpoint-up mixins.

--- test/true.js
+++ test/true.js
@@ -1,7 +1,7 @@
 const { join } = require('path');
 const sassTrue = require('sass-true');
 
-['test-true.scss']
+['test-true.scss', 'test-breakpoints.scss']
   .map((file) => join(__dirname, file))
   .forEach((file) => {
     sassTrue.runSass({ file }, { describe, it });
