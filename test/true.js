b192d19f05284d40039a2dfaa739c169724b4228
Lior Kummer <LKummer41@gmail.com>
2020-05-19 17:32:45 +0300

    Refactor test structure.

--- test/true.js
+++ test/true.js
@@ -1,8 +1,10 @@
 const { join } = require('path');
 const sassTrue = require('sass-true');
 
-['test-break.scss']
-  .map((file) => join(__dirname, file))
-  .forEach((file) => {
-    sassTrue.runSass({ file }, { describe, it });
-  });
+describe('Mixins', () => {
+  ['test-break.scss']
+    .map((file) => join(__dirname, file))
+    .forEach((file) => {
+      sassTrue.runSass({ file }, { describe, it });
+    });
+});
