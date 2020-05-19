bae270f101cc53fd9df1aba07852f1fc65a163f0
Lior Kummer <LKummer41@gmail.com>
2020-05-19 17:33:18 +0300

    Add asserts with tests.
    Add three asserts to be used for input validation.
    Add passing tests using True.
    Add erroring tests using node-sass directly because True does not
    support Sass errors.

---
+++ test/test-asserts.js
@@ -0,0 +1,50 @@
+const assert = require('assert').strict;
+const { join } = require('path');
+
+const sass = require('node-sass');
+const sassTrue = require('sass-true');
+
+describe('Assertions', () => {
+  // In passing cases the assertions can be tested using True.
+  describe('Passing Assertions', () => {
+    const passingAssertions = join(__dirname, 'test-asserts.scss');
+    sassTrue.runSass({ file: passingAssertions }, { describe, it });
+  });
+
+  // True can not test error output, so failing assertions are tested in
+  // isolation.
+  describe('Failing Assertions', () => {
+    describe('key-exists', () => {
+      it('Throws when key does not exist', () => {
+        const data = `
+@import 'src/asserts';
+@include -break-assert-key-exists('key', ());`;
+        assert.throws(() => {
+          sass.renderSync({ data });
+        });
+      });
+    });
+
+    describe('breakpoint-bounds', () => {
+      it('Throws when both bounds are not present', () => {
+        const data = `
+@import 'src/asserts';
+@include -break-assert-breakpoint-bounds(())`;
+        assert.throws(() => {
+          sass.renderSync({ data });
+        });
+      });
+    });
+
+    describe('content', () => {
+      it('Throws when no @content block is passed', () => {
+        const data = `
+@import 'src/asserts';
+@include -break-assert-content;`;
+      });
+      assert.throws(() => {
+        sass.renderSync({ data });
+      });
+    });
+  });
+});
