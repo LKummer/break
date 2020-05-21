fa900f6a3b51bbf20a91b5bcd7301f81e66aae06
Lior Kummer <LKummer41@gmail.com>
2020-05-21 18:42:03 +0300

    Format the project files.

--- test/true.js
+++ test/true.js
@@ -5,10 +5,10 @@ describe('Mixins', () => {
   describe('No keys for highest and lowest', () => {
     const passingAssertions = join(__dirname, 'test-mixins.scss');
     sassTrue.runSass({ file: passingAssertions }, { describe, it });
-  })
+  });
 
   describe('Null values for highest and lowest', () => {
     const passingAssertions = join(__dirname, 'test-mixins-null-edges.scss');
     sassTrue.runSass({ file: passingAssertions }, { describe, it });
-  })
+  });
 });
